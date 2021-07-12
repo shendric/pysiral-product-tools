91dd7f3775095e761f36b6e432a733f382a23e3d
Stefan Hendricks <stefan.hendricks@awi.de>
2021-07-12 12:22:40 +0200

    add own netcdf reader capability

--- pysiral_product_tools/catalog.py
+++ pysiral_product_tools/catalog.py
@@ -11,9 +11,11 @@ import datetime
 import dateutil
 import numpy as np
 
-from pysiral.iotools import ReadNC
+from netCDF4 import Dataset, num2date
+
 from pysiral.logging import DefaultLoggingClass
 from dateperiods import DatePeriod
+from pysiral.output import NCDateNumDef
 from pysiral import psrlcfg
 from pysiral.errorhandler import ErrorStatus
 
@@ -376,6 +378,10 @@ class SIRALProductCatalog(DefaultLoggingClass):
         return sorted(self._catalog.keys())
 
     @property
+    def duration(self):
+        return list(set([item.time_coverage_duration for item in self._catalog.values()]))
+
+    @property
     def period_ids(self):
         return [self._catalog[idstr].period_id for idstr in self.product_ids]
 
@@ -676,3 +682,83 @@ class ProductMetadata(DefaultLoggingClass):
             hemisphere = "global"
 
         return hemisphere
+
+
+class ReadNC(object):
+    """
+    Quick & dirty method to parse content of netCDF file into a python object
+    with attributes from file variables
+    """
+    def __init__(self, filename, verbose=False, autoscale=True,
+                 nan_fill_value=False, global_attrs_only=False):
+        self.error = ErrorStatus()
+        self.time_def = NCDateNumDef()
+        self.parameters = []
+        self.attributes = []
+        self.verbose = verbose
+        self.autoscale = autoscale
+        self.global_attrs_only = global_attrs_only
+        self.nan_fill_value = nan_fill_value
+        self.filename = filename
+        self.parameters = []
+        self.read_globals()
+        self.read_content()
+
+    def read_globals(self):
+        pass
+#        self.gobal_attributes = {}
+#        f = Dataset(self.filename)
+#        print f.ncattrs()
+#        f.close()
+
+    def read_content(self):
+
+        self.keys = []
+
+        # Open the file
+        try:
+            f = Dataset(self.filename)
+        except RuntimeError:
+            msg = "Cannot read netCDF file: %s" % self.filename
+            self.error.add_error("nc-runtime-error", msg)
+            self.error.raise_on_error()
+
+        f.set_auto_scale(self.autoscale)
+
+        # Get the global attributes
+        for attribute_name in f.ncattrs():
+
+            self.attributes.append(attribute_name)
+            attribute_value = getattr(f, attribute_name)
+
+            # Convert timestamps back to datetime objects
+            # TODO: This needs to be handled better
+            if attribute_name in ["start_time", "stop_time"]:
+                attribute_value = num2date(
+                    attribute_value, self.time_def.units,
+                    calendar=self.time_def.calendar)
+            setattr(self, attribute_name, attribute_value)
+
+        # Get the variables
+        if not self.global_attrs_only:
+            for key in f.variables.keys():
+
+                variable = f.variables[key][:]
+
+                try:
+                    is_float = variable.dtype in ["float32", "float64"]
+                    has_mask = hasattr(variable, "mask")
+                except:
+                    is_float, has_mask = False, False
+
+                if self.nan_fill_value and has_mask and is_float:
+                    is_fill_value = np.where(variable.mask)
+                    variable[is_fill_value] = np.nan
+
+                setattr(self, key, variable)
+                self.keys.append(key)
+                self.parameters.append(key)
+                if self.verbose:
+                    print(key)
+            self.parameters = f.variables.keys()
+        f.close()
